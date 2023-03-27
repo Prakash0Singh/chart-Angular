967b5bc87b41f9cd16131a922e4f8ff87c682b6f
Prakash Singh Basnal <72181041+Prakash0Singh@users.noreply.github.com>
2023-03-27 10:13:28 +0530

    Update graphs-charts.component.ts

--- graphs-charts/graphs-charts.component.ts
+++ graphs-charts/graphs-charts.component.ts
@@ -9,10 +9,16 @@ Chart.register(...registerables)
   styleUrls: ['./graphs-charts.component.css']
 })
 export class GraphsChartsComponent implements OnInit {
-  myCharts:any;
-  chartName: any='barChart'
-  
-  constructor(private formBuilder: FormBuilder, private _chartApi: ApiCallingService, private _apiCalling: ApiCallingService) {
+
+    //! For Auth Guard canDeactivate
+    redirectingAllowed=true
+
+  myCharts: any;
+  chartName: any = 'bargraph'
+  deleteUserId: number = 1;
+  editUserId: number = 0;
+
+  constructor(private formBuilder: FormBuilder, private _chartApi: ApiCallingService) {
 
     this.chartForm = this.formBuilder.group({
       name: [''],
@@ -33,47 +39,57 @@ export class GraphsChartsComponent implements OnInit {
 
   barChartData: any = [];
   labeldata: any = [];
-  realdata: any = [];
+  saledata: any = [];
+  purchasedata: any = [];
   colordata: any = [];
+  allUser: any = [];
+  deletingUserName: string = '';
+  showUpdateButton = false
+
+  CurrentChartType: string = 'bar'
 
   ngOnInit(): void {
     this.getData('bar')
+
+    // this.bubbleChartRender()
+    // this.scatterChartRender()
+    // this.lineChartRender()
   }
-  getData(barType:any) {
-    this.labeldata=[];
-    this.colordata=[];
-    this.realdata=[];
+  getData(barType: any) {
+    this.labeldata = [];
+    this.colordata = [];
+    this.saledata = [];
     this._chartApi.chartApiData().subscribe((data) => {
       this.barChartData = data
+      this.allUser = data
       if (this.barChartData != null) {
 
         for (let index = 0; index < this.barChartData.length; index++) {
 
           this.labeldata.push(this.barChartData[index].itemName)
-          this.realdata.push(Number(this.barChartData[index].saleCost))
+          this.saledata.push(Number(this.barChartData[index].saleCost))
+          this.purchasedata.push(Number(this.barChartData[index].purchaseCost))
           this.colordata.push(this.barChartData[index].colorCode)
         }
-        this.renderPieChart(this.labeldata, this.realdata, this.colordata, barType, "showChartHere");
+        this.renderPieChart(this.labeldata, this.saledata, this.purchasedata, this.colordata, barType, "showChartHere");
       }
     })
-    this.bubbleChartRender()
-    this.scatterChartRender()
-    this.lineChartRender()
+
   }
 
-  renderPieChart(labeldata: any, maindata: any, colordata: any, type: any, id: any) {
-    console.log("labeldata",labeldata);
-    console.log("maindata",maindata);
-    console.log("colordata",colordata);
-    console.log("type",type);
-    console.log("id",id);
+  renderPieChart(labeldata: any, saledata: any, purchasedata: any, colordata: any, type: any, id: any) {
+    console.log("labeldata", labeldata);
+    console.log("maindata", saledata);
+    console.log("colordata", colordata);
+    console.log("type", type);
+    console.log("id", id);
 
     var chartExist = Chart.getChart("showChartHere");
     // if (chartExist != undefined) {
-      chartExist?.destroy();
+    chartExist?.destroy();
     // }
 
-  
+
     this.myCharts =
       new Chart('showChartHere', {
         type: type,
@@ -81,7 +97,7 @@ export class GraphsChartsComponent implements OnInit {
           labels: labeldata,
           datasets: [{
             label: 'Sale',
-            data: maindata,
+            data: saledata,
             borderWidth: 1,
             backgroundColor: colordata,
             borderColor: ['black']
@@ -96,10 +112,58 @@ export class GraphsChartsComponent implements OnInit {
         }
       });
 
-      this.myCharts.ty=type;
-      this.myCharts.data.labels=this.labeldata;
-      this.myCharts.data.datasets[0].data=this.realdata;
-      this.myCharts.data.datasets[0].backgroundColor=this.colordata;
+    this.myCharts.ty = type;
+    this.myCharts.data.labels = this.labeldata;
+    this.myCharts.data.datasets[0].data = this.saledata;
+    this.myCharts.data.datasets[0].backgroundColor = this.colordata;
+  }
+
+
+  //*To get form data 
+
+  chartForm: FormGroup;
+
+  //? Submiting Chart Form Function
+  submitChartDetail() {
+    this._chartApi.postNewDetailChart(this.chartForm.value).subscribe((data) => {
+      console.log(data, "Update-------------->");
+      this.chartForm.reset();
+      this.showthisTypeChart(this.chartName)
+    })
+  }
+  // ! radio button array of object
+
+  radioBttons: any[] = [
+    { id: 'bargraph', label: 'Bar Graph', type: 'bar', chec: true },
+    { id: 'piechart', label: 'Pie Chart', type: 'pie', chec: false },
+    { id: 'DoughnutChart', label: 'Doughnut Chart', type: 'doughnut', chec: false },
+    { id: 'RadarChart', label: 'Radar Chart', type: 'radar', chec: false },
+    { id: 'PolarAreaChart', label: 'Polar Area Chart', type: 'polarArea', chec: false }
+  ]
+
+  // ! different charts array of object
+
+  showthisTypeChart(event: any) {
+
+    if (event == this.chartName) {
+      this.chartName = event
+      console.log(this.chartName,"12");
+
+      // this.getData(this.CurrentChartType);
+    }
+    else {
+      console.log(event.target);
+      this.chartName = event.target.id
+      console.log(event.target.checked, "You Clicked -", event.target.id);
+    }
+
+    this.radioBttons.filter(data => {
+      if (data.id == this.chartName) {
+        console.log(data.type);
+        this.getData(data.type);
+        this.CurrentChartType = data.type
+      }
+    })
   }
 
   bubbleChartRender() {
@@ -165,7 +229,7 @@ export class GraphsChartsComponent implements OnInit {
       labels: this.colordata,
       datasets: [{
         label: 'Amount',
-        data: this.realdata,
+        data: this.saledata,
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1
@@ -178,46 +242,42 @@ export class GraphsChartsComponent implements OnInit {
     })
   }
 
-  //*To get form data 
-
-  chartForm: FormGroup;
-
-  //? Submiting Chart Form Function
-  submitChartDetail() {
-    this._apiCalling.postNewDetailChart(this.chartForm.value).subscribe((data) => {
-      console.log(data, "Update-------------->");
-      this.chartForm.reset();
-      this.showthisTypeChart(this.chartName)
-    })
+  showChart() {
+    this.showUpdateButton = false
   }
-  // ! radio button array of object
-
-  radioBttons: any[] = [
-    { id: 'bargraph', label: 'Bar Graph', type: 'bar', chec: true },
-    { id: 'piechart', label: 'Pie Chart', type: 'pie', chec: false },
-    { id: 'DoughnutChart', label: 'Doughnut Chart', type: 'doughnut', chec: false },
-    { id: 'RadarChart', label: 'Radar Chart', type: 'radar', chec: false },
-    { id: 'PolarAreaChart', label: 'Polar Area Chart', type: 'polarArea', chec: false }
-  ]
-
-  // ! different charts array of object
-
-  showthisTypeChart(event: any) {
-    if (event == this.chartName) {
-
-       this.chartName = event
-    }
-    else {
-      console.log(event.target);
-    this.chartName = event.target.id
-      console.log(event.target.checked, "You Clicked -", event.target.id);
-    }
-
-    this.radioBttons.filter(data => {
-      if (data.id == this.chartName) {
-        this.getData(data.type);
-      }
+  // ? To Remove && Update Api Data functions
+  editData(i: any) {
+    this.chartForm.patchValue({
+      name: i.name,
+      itemName: i.itemName,
+      year: i.year,
+      colorCode: i.colorCode,
+      purchaseCost: i.purchaseCost,
+      saleCost: i.saleCost,
     })
+    this.editUserId = i.id;
+    this.showUpdateButton = true
+  }
+  updateChartDeatils() {
+    console.log("You Clicked Update Button");
+    this._chartApi.putChartData(this.editUserId, this.chartForm.value).subscribe((data => {
+      console.log(data);
+      this.chartForm.reset()
+      this.getData(this.CurrentChartType);
+    }))
+  }
+
+  deleteData(id: any, name: any) {
+    console.log(id);
+    this.deleteUserId = id;
+    console.log(name);
+    this.deletingUserName = name;
+  }
+
+  confirmDelete() {
+    this._chartApi.deleteChartData(this.deleteUserId).subscribe((data => {
+      this.getData(this.CurrentChartType);
+    }))
   }
 
 }
